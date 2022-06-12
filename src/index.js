91170c3cc8c4bba39fd043d112c79607f0181721
minhtan <32776120+minhtan4711@users.noreply.github.com>
2022-06-12 17:03:50 +0700

    feature: init project (#1)
    * express config
    
    * integrate dev script

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello World</h1>")
+})
+
+
+app.listen(PORT, () => {
+    console.log(`API is listening on port ${PORT}`)
+})
