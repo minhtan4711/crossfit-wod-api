f9597eb224ff046f23d24ce30afc1efa9d253da7
minhtan <32776120+minhtan4711@users.noreply.github.com>
2022-06-12 17:30:31 +0700

    feature: versioning control (#2)
    * config v1 routes for versioning
    
    * using api v1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 const express = require("express")
+const v1Router = require("./v1/routes")
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.get("/", (req, res) => {
-    res.send("<h1>Hello World</h1>")
-})
+app.use("/api/v1", v1Router)
 
 
 app.listen(PORT, () => {
